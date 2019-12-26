6cb52096899966ce5226d09be071451d789bd58f
qunfm <1642549031@qq.com>
2019-12-26 12:27:13 +0800

    Update test1.py

--- interface/test1.py
+++ interface/test1.py
@@ -1,10 +1,8 @@
 #coding:utf-8
 import requests
 import json
-#r = requests.get('http://www.cnblogs.com/yoyoketang/')
-#par = {"Keywords":"yoyoketang"}
-#r = requests.get('http://zzk.cnblogs.com/s/blogpost?Keywords=yoyoketang',params=par)
-payload = {"yoyo":"hello world","pathonQQ群":"226296743"}
+
+payload = {"test":"hello world","pathonQQ群":"1234567890"}
 #r = requests.get("https://www.baidu.com")
 data_json = json.dumps(payload)
 r = requests.post('http://httpbin.org/post',json=data_json)
