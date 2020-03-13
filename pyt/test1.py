dc2213d078182e104b3a204cdfb698d19c229582
mao <mao@163.com>
2020-03-13 10:58:14 +0800

    j

---
+++ pyt/test1.py
@@ -0,0 +1,15 @@
+import unittest
+import pytest
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)
+def func(x):
+    return x+1
+def test_answer():
+    assert func(3)==5
+
+if __name__ == '__main__':
+    #unittest.main()
+    a= test_answer()
+    print a
